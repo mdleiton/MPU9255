af86aea6307d310bf086c6ccc8dd7c13819344cf
mdleiton <mau_leiton96@hotmail.com>
2018-04-20 15:05:24 -0500

    [ADD] offset IMU

---
+++ offsetIMU.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+  int fd; 
+  wiringPiSetup();
+  fd = wiringPiI2CSetup(0x68);
+
+  int16_t InBuffer[6] = {0}; 
+  float giro_x=0,giro_y=0, giro_z=0;
+  float acc_x=0,acc_y=0, acc_z=0;
+
+  ofstream myfile;
+  myfile.open ("offsetIMU.txt");
+  
+  for (int i=0; i<= 1000;i++){
+
+	//datos acelerómetro
+    InBuffer[0]=  (wiringPiI2CReadReg8 (fd, 0x3B)<<8)|wiringPiI2CReadReg8 (fd, 0x3C);
+    InBuffer[1]=  (wiringPiI2CReadReg8 (fd, 0x3D)<<8)|wiringPiI2CReadReg8 (fd, 0x3E);
+    InBuffer[2]=  (wiringPiI2CReadReg8 (fd, 0x3F)<<8)|wiringPiI2CReadReg8 (fd, 0x40);   
+    
+    acc_x = acc_x + InBuffer[0];
+    acc_y = acc_y + InBuffer[1];
+    acc_z = acc_z + InBuffer[2];
+
+    //datos giroscopio
+    InBuffer[3]=  (wiringPiI2CReadReg8 (fd, 0x43)<<8)|wiringPiI2CReadReg8 (fd, 0x44);
+    InBuffer[4]=  (wiringPiI2CReadReg8 (fd, 0x45)<<8)|wiringPiI2CReadReg8 (fd, 0x46);
+    InBuffer[5]=  (wiringPiI2CReadReg8 (fd, 0x47)<<8)|wiringPiI2CReadReg8 (fd, 0x48); 
+
+    giro_x = giro_x + InBuffer[3];
+    giro_y = giro_y + InBuffer[4];
+    giro_z = giro_z + InBuffer[5]; 
+
+  }
+
+  giro_x /= 1000;
+  giro_y /= 1000;
+  giro_z /= 1000;
+
+  acc_x /= 1000;
+  acc_y /= 1000;
+  acc_z = (acc_z / 1000) -16384 ;
+
+  myfile << giro_x << "," << giro_y << "," << giro_z << "," << acc_x << "," << acc_y << "," << acc_z << endl;
+  myfile.close();
+
+  return 0;
+}
+
